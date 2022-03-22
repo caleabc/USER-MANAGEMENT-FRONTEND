d0958311b199bd3dfd29ba65daf8fa087024451b
Christian Cale <christiansilvoza@yahoo.com>
2022-03-22 13:26:12 +0800

    ETH operation

--- src/pages/home.js
+++ src/pages/home.js
@@ -734,42 +734,64 @@ function Home() {
                         </p>
                     </div>
 
-                    <div
-                        style={{
-                            marginTop: "1rem",
-                            border: "2px solid gray",
-                            borderRadius: "5px",
-                            padding: "1rem",
-                            fontFamily: "Montserrat",
-                        }}
-                    >
-                        <div style={{display: "flex"}}>
-                            <div style={{width: "35%",}}>Date</div>
-                            <div style={{
-                                width: "20%",
-                                paddingLeft: "4px",
-                                paddingRight: "10px"
-                            }}>Action
-                            </div>
-                            <div style={{width: "45%"}}>Amount</div>
+                    {currentUser == undefined &&
+                        <div
+                            style={{
+                                marginTop: "1rem",
+                                border: "2px solid gray",
+                                borderRadius: "5px",
+                                padding: "1rem",
+                                fontFamily: "Montserrat",
+                            }}
+                        >
+                            History will be here
                         </div>
-                        {ethData.map((t) =>
-                            <div style={{display: "flex", fontSize: ".8rem"}}>
-                                <div
-                                    style={{width: "35%",}}>{new Date(Number(t["timeStamp"])).toString().slice(3, 16)}</div>
+                    }
+
+                    {currentUser !== undefined &&
+                        <div
+                            style={{
+                                marginTop: "1rem",
+                                border: "2px solid gray",
+                                borderRadius: "5px",
+                                padding: "1rem",
+                                fontFamily: "Montserrat",
+                            }}
+                        >
+                            <div style={{display: "flex", marginBottom: "1rem"}}>
+                                <div style={{width: "35%",}}>Date</div>
                                 <div style={{
                                     width: "20%",
                                     paddingLeft: "4px",
                                     paddingRight: "10px"
-                                }}>Transfer
+                                }}>Action
                                 </div>
-                                <div style={{width: "45%"}}>{ethers.utils.formatEther(t["value"]).substring(0, 10)}...
-                                </div>
+                                <div style={{width: "45%"}}>Amount</div>
                             </div>
-                        )}
-
-
-                    </div>
+                            {ethData.map((t) =>
+                                <div style={{
+                                    display: "flex",
+                                    fontSize: ".8rem",
+                                    marginBottom: "1rem",
+                                    backgroundColor: "lightgray",
+                                    padding: ".5rem",
+                                    borderRadius: "1rem"
+                                }}>
+                                    <div
+                                        style={{width: "35%",}}>{new Date(Number(t["timeStamp"])).toString().slice(3, 16)}</div>
+                                    <div style={{
+                                        width: "20%",
+                                        paddingLeft: "4px",
+                                        paddingRight: "10px"
+                                    }}>Transfer
+                                    </div>
+                                    <div style={{width: "45%"}}>{ethers.utils.formatEther(t["value"]).substring(0, 10)}...
+                                    </div>
+                                </div>
+                            )}
+                            
+                        </div>
+                    }
                 </Cell>
             </Grid>
         </>
